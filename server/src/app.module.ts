5b9cdd1968f32752f92997952596aca35a96533d
vladimirSEOadv <vladimirSEOadv@gmail.com>
2023-11-08 03:31:11 +0300

    create auth module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { CategoryModule } from './category/category.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UserModule, CategoryModule],
+  imports: [UserModule, CategoryModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
