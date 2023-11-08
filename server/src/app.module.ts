ca532d49eb1eb2885f6aa2815b713fd044327c4d
vladimirSEOadv <vladimirSEOadv@gmail.com>
2023-11-08 03:32:13 +0300

    create transaction module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { CategoryModule } from './category/category.module';
 import { AuthModule } from './auth/auth.module';
+import { TransactionModule } from './transaction/transaction.module';
 
 @Module({
-  imports: [UserModule, CategoryModule, AuthModule],
+  imports: [UserModule, CategoryModule, AuthModule, TransactionModule],
   controllers: [AppController],
   providers: [AppService],
 })
