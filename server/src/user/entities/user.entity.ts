c1848a4d7616a770c9f33f55604b295f410694cf
vladimirSEOadv <vladimirSEOadv@gmail.com>
2023-11-08 07:30:16 +0300

    added tables user and category

--- server/src/user/entities/user.entity.ts
+++ server/src/user/entities/user.entity.ts
@@ -1,4 +1,30 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { Category } from '../../category/entities/category.entity';
+
+@Entity()
 export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
   email: string;
+
+  @Column()
   password: string;
+
+  @OneToMany(() => Category, (category) => category.user)
+  categories: Category[];
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
 }
