b49dbaf999e7c484c6c17ef0f7bcea5f48a05481
vladimirSEOadv <vladimirSEOadv@gmail.com>
2023-11-08 03:21:39 +0300

    create category module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { CategoryModule } from './category/category.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, CategoryModule],
   controllers: [AppController],
   providers: [AppService],
 })
