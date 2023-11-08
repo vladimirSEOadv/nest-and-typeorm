c035e18473f050a1dfbeea20e488d77863488fc8
vladimirSEOadv <vladimirSEOadv@gmail.com>
2023-11-08 07:55:41 +0300

    added table transaction

--- server/src/transaction/entities/transaction.entity.ts
+++ server/src/transaction/entities/transaction.entity.ts
@@ -1,1 +1,37 @@
-export class Transaction {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { User } from '../../user/entities/user.entity';
+import { Category } from '../../category/entities/category.entity';
+
+@Entity()
+export class Transaction {
+  @PrimaryColumn({ name: 'transaction_id' })
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column({ nullable: true })
+  type: string;
+
+  @ManyToOne(() => User, (user) => user.transactions)
+  @JoinColumn({ name: 'user_id' })
+  user: User;
+
+  @ManyToOne(() => Category, (category) => category.transactions)
+  @JoinColumn({ name: 'category_id' })
+  category: Category;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
