6bb957fe4522243ca7106facf6f0890c5765b270
vladimirSEOadv <vladimirSEOadv@gmail.com>
2023-11-08 03:18:58 +0300

    create UserModule

--- server/src/user/user.controller.ts
+++ server/src/user/user.controller.ts
@@ -11,6 +11,7 @@ import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
+// http://localhost:3001/api/user
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
@@ -25,16 +26,19 @@ export class UserController {
     return this.userService.findAll();
   }
 
+  // http://localhost:3001/api/user/52
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.userService.findOne(+id);
   }
 
+  // http://localhost:3001/api/user/52
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.userService.update(+id, updateUserDto);
   }
 
+  // http://localhost:3001/api/user/52
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.userService.remove(+id);
