082040925b7a116a34f29f885db4f8ac98f8bc8d
vladimirSEOadv <vladimirSEOadv@gmail.com>
2023-11-08 05:00:44 +0300

    added @nestjs/config package

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -5,9 +5,16 @@ import { UserModule } from './user/user.module';
 import { CategoryModule } from './category/category.module';
 import { AuthModule } from './auth/auth.module';
 import { TransactionModule } from './transaction/transaction.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule, CategoryModule, AuthModule, TransactionModule],
+  imports: [
+    UserModule,
+    CategoryModule,
+    AuthModule,
+    TransactionModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
