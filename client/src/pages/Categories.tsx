79e7e34d710068b962127e83184c3354fbfe7cb7
vladimirSEOadv <vladimirSEOadv@gmail.com>
2023-11-19 14:11:45 +0300

    add pages

---
+++ client/src/pages/Categories.tsx
@@ -0,0 +1,5 @@
+import { FC } from "react";
+
+export const Categories: FC = () => {
+  return <div>Categories</div>;
+};
---
+++ client/src/pages/Layout.tsx
@@ -0,0 +1,5 @@
+import { FC } from "react";
+
+export const Layout: FC = () => {
+  return <div>Layout</div>;
+};
