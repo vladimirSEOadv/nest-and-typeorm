cce6c1016cc5b99fb07bc515f02d62bc3c1131c2
vladimirSEOadv <vladimirSEOadv@gmail.com>
2023-11-21 11:10:30 +0300

    remove checks for unused variables

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -14,5 +14,7 @@ module.exports = {
             'warn',
             {allowConstantExport: true},
         ],
+        'no-unused-vars': 'off', // TODO turn on checks for unused variables
+        '@typescript-eslint/no-unused-vars': 'off', // TODO turn on checks for unused variables
     },
 }
